eac9ea52db2c66daf37922735770d2ad24e4bd83
balatzar <bataprod@gmail.com>
2020-04-17 19:52:23 +0200

    Init

---
+++ index.js
@@ -0,0 +1,7 @@
+(function () {
+  var daysSince = Math.floor(
+    ((new Date("2020/03/17") - new Date()) / 1000 / 60 / 60 / 24) * -1
+  );
+  document.querySelector(".js-displayText").innerHTML =
+    "Le confinement dure depuis " + daysSince + " jours";
+})();
