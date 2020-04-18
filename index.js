79ca5239e8473f5b4dcda27289770024e20e56d0
balatzar <bataprod@gmail.com>
2020-04-18 13:20:01 +0200

    Add minimal css

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
     ((new Date("2020/03/17") - new Date()) / 1000 / 60 / 60 / 24) * -1
   );
   document.querySelector(".js-displayText").innerHTML =
-    "Le confinement dure depuis " + daysSince + " jours";
+    "Le confinement dure depuis <span class='days-since'>" +
+    daysSince +
+    "</span> jours";
 })();
