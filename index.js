7927e659e38823e4e5270e6fd15192682192daf5
balatzar <bataprod@gmail.com>
2020-04-19 20:12:55 +0200

    New design

--- index.js
+++ index.js
@@ -3,7 +3,19 @@
     ((new Date("2020/03/17") - new Date()) / 1000 / 60 / 60 / 24) * -1
   );
   document.querySelector(".js-displayText").innerHTML =
-    "Le confinement dure depuis <span class='days-since'>" +
+    "<span class='days-since'>" +
     daysSince +
-    "</span> jours";
+    "</span> jours depuis le début du confinement";
+
+  var daysRemaining = Math.floor(
+    (( new Date() - new Date("2020/05/11")) / 1000 / 60 / 60 / 24) * -1
+  );
+  document.querySelector(".js-displaySecondaryText").innerHTML =
+    "Jours de confinement prévus : <span class='days-remain'>" +
+    daysRemaining +
+    "</span>";
+    
+  document.querySelector(".js-share button").addEventListener("click", function() {
+    document.querySelector(".js-share p").innerHTML = "L'URL de cette page a ete copiee !"
+   })
 })();
